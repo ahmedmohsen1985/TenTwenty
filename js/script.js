3893e36a49c7f6bdc24c08dd610d6bfa978c6a7b
Ahmed.mohsen <ahmed.mohsen@akqa.com>
2023-05-17 03:23:50 +0300

    Hero section

--- js/script.js
+++ js/script.js
@@ -1,31 +1,83 @@
 $(document).ready(function () {
 
-    // Scroll To ===================== 
-    $(".scrollTo").on("click", function (e) {
-        e.preventDefault();
-        var target = $(this).attr('href');
-        //var headerHeight = $('header').outerHeight();
-        $('html, body').animate({
-            scrollTop: ($(target).offset().top - 106)
-        });
+    var time = 2;
+    var $bar,
+        $slick,
+        tick
+
+    var $slick = $('.hero_slider');
+    var $status = $('.pagingInfo');
+
+
+    $slick.on('init reInit afterChange', function (event, slick, currentSlide, nextSlide) {
+    var i = (currentSlide ? currentSlide : 0) + 1;
+    $status.html('<span>' + i + '</span>' + '/' + slick.slideCount);
     });
 
-    // Fixed Header ===================== 
-    $(window).scroll(function () {
-        var sticky = $('body.sticky-me'),
-            scroll = $(window).scrollTop();
+    $slick.slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 4000,
+        prevArrow: false,
+        nextArrow: '<button type="button" class="slick-custom-buttom slick-next"><div data-slick-index="0" class="progressBar"></div><div class="pagingInfo"></div></button>',
+        useTransform: false,
+        infinite: true,
+        speed: 300,
+        fade: true,
+        cssEase: 'linear',
+        pauseOnHover: false, 
+    });
+    setTimeout(function () {
+        $('.slick-next').append('<span>Next</span><div class="next-slick-img slick-thumb-nav"><img src="/next.jpg" class="thumb"></div>');
+        get_next_slick_img();
+    }, 500);
 
-        if (scroll >= 1)
-            sticky.addClass('fixed-header');
-        else
-            sticky.removeClass('fixed-header');
-
-        if ($(window).width() < 991) {
-            if (scroll >= 1)
-                sticky.addClass('fixed-header');
-            else
-                sticky.removeClass('fixed-header');
+    $slick.on('swipe', function(event, slick, direction) {
+        if (direction == 'left') {
+            get_next_slick_img();
         }
+        else {
+          get_next_slick_img();
+        }
+    });
+    
+    $slick.on('click', '.slick-next', function () {
+        get_next_slick_img();
+    });
+    
+    function get_next_slick_img() {
+        var next_slick_img = $('.slick-current').next('.slider-image').find('img').attr('src');
+        $('.next-slick-img img').attr('src', next_slick_img);
+        $('.next-slick-img').css('background-image', 'url(' + next_slick_img + ')');
+    }
+
+
+    $bar = $('.progressBar');
+
+    function startProgressbar() {
+        resetProgressbar();
+        tick = setInterval(interval, 30);
+    }
+
+    function interval() {
+        $bar.css({
+            Animation: "progress 4s linear forwards"
+          });
+      }
+    
+      function resetProgressbar() {
+        $bar.css({
+            Animation: "stop 4s linear forwards"
+        });
+        clearTimeout(tick);
+      }
+    
+
+    startProgressbar();
+
+    $slick.on('beforeChange', function (event, slick, currentSlide, nextSlide) {
+        startProgressbar();
     });
 
 });
