48cb98c3463874a323c769e46ca6f94caa319d5d
Ahmed.mohsen <ahmed.mohsen@akqa.com>
2023-05-18 01:40:55 +0300

    Script file fixed

--- js/script.js
+++ js/script.js
@@ -9,8 +9,8 @@ $(document).ready(function () {
 
 
     $slick.on('init reInit afterChange', function (event, slick, currentSlide, nextSlide) {
-    var i = (currentSlide ? currentSlide : 0) + 1;
-    $status.html('<span>0' + i + '</span>' + '<span class="line"></span> 0' + slick.slideCount);
+        var i = (currentSlide ? currentSlide : 0) + 1;
+        $status.html('<span>0' + i + '</span>' + '<span class="line"></span> 0' + slick.slideCount);
     });
 
     $slick.slick({
@@ -20,31 +20,31 @@ $(document).ready(function () {
         autoplaySpeed: 4000,
         prevArrow: false,
         nextArrow: '<button type="button" class="slick-custom-buttom slick-next"><div data-slick-index="0" class="progressBar"></div></button>',
-        useTransform: false,
+        // useTransform: false,
         infinite: true,
         speed: 300,
         fade: true,
         cssEase: 'linear',
-        pauseOnHover: false, 
+        pauseOnHover: false,
     });
     setTimeout(function () {
         $('.slick-next').append('<span>Next</span><div class="next-slick-img slick-thumb-nav"><img src="/next.jpg" class="thumb"></div>');
         get_next_slick_img();
     }, 500);
 
-    $slick.on('swipe', function(event, slick, direction) {
+    $slick.on('swipe', function (event, slick, direction) {
         if (direction == 'left') {
             get_next_slick_img();
         }
         else {
-          get_next_slick_img();
+            get_next_slick_img();
         }
     });
-    
+
     $slick.on('click', '.slick-next', function () {
         get_next_slick_img();
     });
-    
+
     function get_next_slick_img() {
         var next_slick_img = $('.slick-current').next('.slider-image').find('img').attr('src');
         $('.next-slick-img img').attr('src', next_slick_img);
@@ -62,16 +62,16 @@ $(document).ready(function () {
     function interval() {
         $bar.css({
             Animation: "progress 4s linear forwards"
-          });
-      }
-    
-      function resetProgressbar() {
+        });
+    }
+
+    function resetProgressbar() {
         $bar.css({
             Animation: "stop 4s linear forwards"
         });
         clearTimeout(tick);
-      }
-    
+    }
+
 
     startProgressbar();
 
@@ -79,14 +79,14 @@ $(document).ready(function () {
         startProgressbar();
     });
 
-    $('.menuBtn').click(function() {
-		$(this).toggleClass('act');
-			if($(this).hasClass('act')) {
-				$('.mainMenu').addClass('act');
-			}
-			else {
-				$('.mainMenu').removeClass('act');
-			}
-	});
+    $('.menuBtn').click(function () {
+        $(this).toggleClass('act');
+        if ($(this).hasClass('act')) {
+            $('.mainMenu').addClass('act');
+        }
+        else {
+            $('.mainMenu').removeClass('act');
+        }
+    });
 
 });
