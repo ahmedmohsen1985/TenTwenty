bf4d975254fbfda308ccbb7b59c6892d0822ca56
Ahmed.mohsen <ahmed.mohsen@akqa.com>
2023-05-18 23:44:02 +0300

    Second Slider

--- js/script.js
+++ js/script.js
@@ -1,5 +1,19 @@
 $(document).ready(function () {
 
+    // Nav Menu for the Mobile version 414px
+
+    $('.menuBtn').click(function () {
+        $(this).toggleClass('act');
+        if ($(this).hasClass('act')) {
+            $('.mainMenu').addClass('act');
+        }
+        else {
+            $('.mainMenu').removeClass('act');
+        }
+    });
+
+    // Hero Slider
+
     var $bar,
         $slick,
         tick
@@ -8,9 +22,9 @@ $(document).ready(function () {
     var $status = $('.pagingInfo');
 
 
-    $slick.on('init reInit afterChange', function (event, slick, currentSlide, nextSlide) {
+    $slick.on('init reInit afterChange', function (e, slick, currentSlide) {
         var i = (currentSlide ? currentSlide : 0) + 1;
-        $status.html('<span>0' + i + '</span>' + '<span class="line"></span> 0' + slick.slideCount);
+        $status.html('<span class="slideNumber">0' + i + '</span>' + '<span class="line"></span> 0' + slick.slideCount);
     });
 
     $slick.slick({
@@ -20,28 +34,31 @@ $(document).ready(function () {
         autoplaySpeed: 4000,
         prevArrow: false,
         nextArrow: '<button type="button" class="slick-custom-buttom slick-next"><div data-slick-index="0" class="progressBar"></div></button>',
-        // useTransform: false,
+        useTransform: false,
         infinite: true,
         speed: 300,
         fade: true,
         cssEase: 'linear',
         pauseOnHover: false,
+        swipe: false
     });
+
+    //--- Hero Slider - Next Thumb
+
     setTimeout(function () {
         $('.slick-next').append('<span>Next</span><div class="next-slick-img slick-thumb-nav"><img src="/next.jpg" class="thumb"></div>');
         get_next_slick_img();
     }, 500);
 
-    $slick.on('swipe', function (event, slick, direction) {
-        if (direction == 'left') {
-            get_next_slick_img();
-        }
-        else {
-            get_next_slick_img();
-        }
+    $slick.on('swipe', function (e) {
+        get_next_slick_img();
     });
 
-    $slick.on('click', '.slick-next', function () {
+    $slick.on('change', '.slick-next', function (e) {
+        get_next_slick_img();
+    });
+
+    $slick.on('click', '.slick-next', function (e) {
         get_next_slick_img();
     });
 
@@ -51,6 +68,7 @@ $(document).ready(function () {
         $('.next-slick-img').css('background-image', 'url(' + next_slick_img + ')');
     }
 
+    //--- Hero Slider - Progress Bar 
 
     $bar = $('.progressBar');
 
@@ -75,18 +93,95 @@ $(document).ready(function () {
 
     startProgressbar();
 
-    $slick.on('beforeChange', function (event, slick, currentSlide, nextSlide) {
+    $slick.on('beforeChange', function () {
         startProgressbar();
     });
 
-    $('.menuBtn').click(function () {
-        $(this).toggleClass('act');
-        if ($(this).hasClass('act')) {
-            $('.mainMenu').addClass('act');
-        }
-        else {
-            $('.mainMenu').removeClass('act');
-        }
+    // Swiper the product slider 
+
+    const swiperImages = new Swiper('.products-items', {
+        direction: 'horizontal',
+        slidesPerView: 3,
+        centeredSlides: true,
+        initialSlide: 2,
+        spaceBetween: 30,
+        breakpoints: {
+            640: {
+                spaceBetween: 30
+            },
+            768: {
+                spaceBetween: 60
+            },
+            1024: {
+                spaceBetween: 180
+            },
+        },
     });
+
+    const swiperContent = new Swiper('.products-content', {
+        spaceBetween: 30,
+        effect: "fade",
+        allowTouchMove: false
+    });
+    
+    swiperImages.controller.control = swiperContent;
+
+    // Cursor
+
+    var cursor = document.querySelector('.cursor');
+    var cursorinner = document.querySelector('.cursor2');
+    var a = document.querySelectorAll('a');
+    var button = document.querySelectorAll('button');
+    var img = document.querySelectorAll('.swiper-slide img');
+
+    document.addEventListener('mousemove', function (e) {
+        var x = e.clientX;
+        var y = e.clientY;
+        cursor.style.transform = `translate3d(calc(${e.clientX}px - 50%), calc(${e.clientY}px - 50%), 0)`
+    });
+
+    document.addEventListener('mousemove', function (e) {
+        var x = e.clientX;
+        var y = e.clientY;
+        cursorinner.style.left = x + 'px';
+        cursorinner.style.top = y + 'px';
+    });
+
+    document.addEventListener('mousedown', function () {
+        cursor.classList.add('click');
+        cursorinner.classList.add('cursorinnerhover')
+    });
+
+    document.addEventListener('mouseup', function () {
+        cursor.classList.remove('click')
+        cursorinner.classList.remove('cursorinnerhover')
+    });
+
+    a.forEach(item => {
+        item.addEventListener('mouseover', () => {
+            cursor.classList.add('text-hover');
+        });
+        item.addEventListener('mouseleave', () => {
+            cursor.classList.remove('text-hover');
+        });
+    })
+
+    button.forEach(item => {
+        item.addEventListener('mouseover', () => {
+            cursor.classList.add('text-hover');
+        });
+        item.addEventListener('mouseleave', () => {
+            cursor.classList.remove('text-hover');
+        });
+    })
+
+    img.forEach(item => {
+        item.addEventListener('mouseover', () => {
+            cursor.classList.add('hover');
+        });
+        item.addEventListener('mouseleave', () => {
+            cursor.classList.remove('hover');
+        });
+    })
 
 });
